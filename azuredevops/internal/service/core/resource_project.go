87bf5fc949b0bb09a308ab2fb50aadac68a7759c
Brendan Thompson <github@blt.is>
2022-09-14 13:39:04 +1000

    feat: add ability to init default repo

--- azuredevops/internal/service/core/resource_project.go
+++ azuredevops/internal/service/core/resource_project.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/core"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/featuremanagement"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/git"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v6/operations"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/client"
 	"github.com/microsoft/terraform-provider-azuredevops/azuredevops/internal/utils"
@@ -24,6 +25,24 @@ import (
 // timeout used to wait for operations on projects to finish before executing an update or delete
 var projectBusyTimeoutDuration time.Duration = 6
 var projectRetryTimeoutDuration time.Duration = 3
+
+// RepoInitType strategy for initializing the repo
+type RepoInitType string
+
+type repoInitTypeValuesType struct {
+	Uninitialized RepoInitType
+	Clean         RepoInitType
+	Fork          RepoInitType
+	Import        RepoInitType
+}
+
+// RepoInitTypeValues enum of strategy for initializing the repo
+var RepoInitTypeValues = repoInitTypeValuesType{
+	Uninitialized: "Uninitialized",
+	Clean:         "Clean",
+	Fork:          "Fork",
+	Import:        "Import",
+}
 
 // ResourceProject schema and implementation for project resource
 func ResourceProject() *schema.Resource {
@@ -94,13 +113,72 @@ func ResourceProject() *schema.Resource {
 					Type: schema.TypeString,
 				},
 			},
+			"initialization": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				MinItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"init_type": {
+							Type:     schema.TypeString,
+							Required: true,
+							ForceNew: true,
+							ValidateFunc: validation.StringInSlice([]string{
+								string(RepoInitTypeValues.Clean),
+								string(RepoInitTypeValues.Fork),
+								string(RepoInitTypeValues.Import),
+								string(RepoInitTypeValues.Uninitialized),
+							}, false),
+						},
+						"source_type": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							ValidateFunc: validation.StringInSlice([]string{"Git"}, false),
+							RequiredWith: []string{"initialization.0.source_url"},
+						},
+						"source_url": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							Default:      "",
+							RequiredWith: []string{"initialization.0.source_type"},
+							ValidateFunc: validation.IsURLWithHTTPorHTTPS,
+						},
+						"service_connection_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+							RequiredWith: []string{
+								"initialization.0.source_url",
+								"initialization.0.source_type",
+							},
+							Default: "",
+						},
+						"default_branch": {
+							Type: schema.TypeString,
+							Optional: true,
+							Default: "main",
+						},
+					},
+				},
+			},
 		},
 	}
 }
 
+// A helper type that is used for transient info only used during repo creation
+type repoInitializationMeta struct {
+	initType            string
+	sourceType          string
+	sourceURL           string
+	serviceConnectionID string
+	defaultBranch string
+}
+
 func resourceProjectCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	clients := m.(*client.AggregatedClient)
-	project, err := expandProject(clients, d, true)
+	project, initialization, err := expandProject(clients, d, true)
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("Error converting terraform data model to Azure DevOps project reference: %+v", err))
 	}
@@ -113,6 +191,47 @@ func resourceProjectCreate(ctx context.Context, d *schema.ResourceData, m interf
 	featureStates, ok := d.GetOk("features")
 	if ok {
 		err = configureProjectFeatures(clients, "", *project.Name, &featureStates, d.Timeout(schema.TimeoutDelete))
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	repo, err := getDefaultRepository(clients, project.Name)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if initialization != nil && strings.EqualFold(initialization.initType, string(RepoInitTypeValues.Import)) &&
+		strings.EqualFold(initialization.sourceType, "Git") {
+		importRequest := git.GitImportRequest{
+			Parameters: &git.GitImportRequestParameters{
+				GitSource: &git.GitImportGitSource{
+					Url: &initialization.sourceURL,
+				},
+			},
+			Repository: repo,
+		}
+
+		if initialization.serviceConnectionID != "" {
+			importRequest.Parameters.ServiceEndpointId = converter.UUID(initialization.serviceConnectionID)
+			importRequest.Parameters.DeleteServiceEndpointAfterImportIsDone = converter.Bool(false)
+		}
+
+		_, importErr := createImportRequest(clients, importRequest, *project.Name, *repo.Name)
+		if importErr != nil {
+			return diag.FromErr(fmt.Errorf("Error import repository in Azure DevOps: %+v ", importErr))
+		}
+	}
+
+	if initialization != nil && strings.EqualFold(initialization.initType, string(RepoInitTypeValues.Clean)) {
+		err = initializeGitRepository(clients, repo, converter.String(fmt.Sprintf("refs/heads/%s", initialization.defaultBranch)))
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("Error initializing repository in Azure DevOps: %+v", err))
+		}
+	}
+
+	if initialization != nil && !(strings.EqualFold(initialization.initType, string(RepoInitTypeValues.Uninitialized))) {
+		err := waitForBranch(clients, project.Name)
 		if err != nil {
 			return diag.FromErr(err)
 		}
@@ -149,7 +268,100 @@ func createProject(clients *client.AggregatedClient, project *core.TeamProject, 
 	if _, err := stateConf.WaitForStateContext(clients.Ctx); err != nil {
 		return fmt.Errorf(" waiting for project ready. %v ", err)
 	}
+
 	return nil
+}
+
+func getDefaultRepository(clients *client.AggregatedClient, project *string) (*git.GitRepository, error) {
+	args := git.GetRepositoryArgs{
+		RepositoryId: project,
+		Project:      project,
+	}
+
+	gitRepo, err := clients.GitReposClient.GetRepository(clients.Ctx, args)
+	if err != nil {
+		return nil, err
+	}
+
+	return gitRepo, nil
+}
+
+func waitForBranch(clients *client.AggregatedClient, projectName *string) error {
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"Waiting"},
+		Target:  []string{"Synched"},
+		Refresh: func() (interface{}, string, error) {
+			state := "Waiting"
+			gitRepo, err := getDefaultRepository(clients, projectName)
+			if err != nil {
+				return nil, "", fmt.Errorf("Error reading repository: %+v", err)
+			}
+
+			if converter.ToString(gitRepo.DefaultBranch, "") != "" {
+				state = "Synched"
+			}
+
+			return state, state, nil
+		},
+		Timeout:                   60 * time.Second,
+		MinTimeout:                2 * time.Second,
+		Delay:                     1 * time.Second,
+		ContinuousTargetOccurence: 1,
+	}
+	if _, err := stateConf.WaitForState(); err != nil { //nolint:staticcheck
+		return fmt.Errorf("Error retrieving expected branch for repository [%s]: %+v", *projectName, err)
+	}
+	return nil
+}
+
+func createImportRequest(clients *client.AggregatedClient, gitImportRequest git.GitImportRequest, project string, repositoryID string) (*git.GitImportRequest, error) {
+	args := git.CreateImportRequestArgs{
+		ImportRequest: &gitImportRequest,
+		Project:       &project,
+		RepositoryId:  &repositoryID,
+	}
+
+	return clients.GitReposClient.CreateImportRequest(clients.Ctx, args)
+}
+
+func initializeGitRepository(clients *client.AggregatedClient, repo *git.GitRepository, defaultBranch *string) error {
+	branchName := converter.ToString(defaultBranch, "")
+	if strings.EqualFold(branchName, "") {
+		branchName = "refs/heads/main"
+	}
+	args := git.CreatePushArgs{
+		RepositoryId: repo.Name,
+		Project:      repo.Project.Name,
+		Push: &git.GitPush{
+			RefUpdates: &[]git.GitRefUpdate{
+				{
+					Name:        converter.String(branchName),
+					OldObjectId: converter.String("0000000000000000000000000000000000000000"),
+				},
+			},
+			Commits: &[]git.GitCommitRef{
+				{
+					Comment: converter.String("Initial commit."),
+					Changes: &[]interface{}{
+						git.Change{
+							ChangeType: &git.VersionControlChangeTypeValues.Add,
+							Item: git.GitItem{
+								Path: converter.String("/readme.md"),
+							},
+							NewContent: &git.ItemContent{
+								ContentType: &git.ItemContentTypeValues.RawText,
+								Content:     repo.Project.Name,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err := clients.GitReposClient.CreatePush(clients.Ctx, args)
+
+	return err
 }
 
 // Configure projects features for a project. If projectID is "" then the projectName will be used to locate (read) the project
@@ -250,7 +462,7 @@ func projectRead(clients *client.AggregatedClient, projectID string, projectName
 
 func resourceProjectUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	clients := m.(*client.AggregatedClient)
-	project, err := expandProject(clients, d, false)
+	project, _, err := expandProject(clients, d, false)
 	if err != nil {
 		return diag.FromErr(fmt.Errorf(" converting terraform data model to AzDO project reference: %+v", err))
 	}
@@ -415,11 +627,11 @@ func deleteProject(clients *client.AggregatedClient, id string, timeoutSeconds t
 }
 
 // Convert internal Terraform data structure to an AzDO data structure
-func expandProject(clients *client.AggregatedClient, d *schema.ResourceData, forCreate bool) (*core.TeamProject, error) {
+func expandProject(clients *client.AggregatedClient, d *schema.ResourceData, forCreate bool) (*core.TeamProject, *repoInitializationMeta, error) {
 	workItemTemplate := d.Get("work_item_template").(string)
 	processTemplateID, err := lookupProcessTemplateID(clients, workItemTemplate)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// an "error" is OK here as it is expected in the case that the ID is not set in the resource data
@@ -451,7 +663,30 @@ func expandProject(clients *client.AggregatedClient, d *schema.ResourceData, for
 		Capabilities: capabilities,
 	}
 
-	return project, nil
+	var initialization *repoInitializationMeta = nil
+	initData := d.Get("initialization").([]interface{})
+	// Note: If configured, this will be of length 1 based on the schema definition above.
+	if len(initData) == 1 {
+		initValues := initData[0].(map[string]interface{})
+
+		initialization = &repoInitializationMeta{
+			initType:            initValues["init_type"].(string),
+			sourceType:          initValues["source_type"].(string),
+			sourceURL:           initValues["source_url"].(string),
+			serviceConnectionID: initValues["service_connection_id"].(string),
+			defaultBranch: initValues["default_branch"].(string),
+		}
+
+		if strings.EqualFold(initialization.initType, "clean") {
+			initialization.sourceType = ""
+			initialization.sourceURL = ""
+			initialization.serviceConnectionID = ""
+		}
+	} else if len(initData) > 1 {
+		return nil, nil, fmt.Errorf("Multiple initialization blocks")
+	}
+
+	return project, initialization, nil
 }
 
 func flattenProject(clients *client.AggregatedClient, d *schema.ResourceData, project *core.TeamProject) error {
